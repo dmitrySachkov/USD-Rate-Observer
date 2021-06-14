764084b12a4015060b72cc619523fda3c52bce68
dmitrySachkov <44116512+dmitrySachkov@users.noreply.github.com>
2021-06-14 23:33:44 +0300

    Initial Commit

---
+++ USD Rate Observer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  USD Rate Observer
+//
+//  Created by Dmitry Sachkov on 14.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
